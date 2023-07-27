d16bd76a3e3ee0a90a27a007cfed622bce3c8d43
Abel-B3lla <bel8794@gmail.com>
2023-07-27 21:59:20 +0300

    update

--- JS/metaJS.js
+++ JS/metaJS.js
@@ -1,13 +1,27 @@
-console.log(`Hello World!`);
-var catName = `pepper`;
+// console.log(`Hello World!`);
+// var catName = `pepper`;
 
-function letterFinder(word, match) {
-  for (var i = 0; i < word.length; i++) {
-    if (word[i] == match) {
-      console.log("Found the", match, "at", i);
-    } else {
-      console.log(`no match found at`, i);
-    }
-  }
+// function letterFinder(word, match) {
+//   for (var i = 0; i < word.length; i++) {
+//     if (word[i] == match) {
+//       console.log("Found the", match, "at", i);
+//     } else {
+//       console.log(`no match found at`, i);
+//     }
+//   }
+// }
+// letterFinder(`test`, `t`);
+
+function consoleStyler(color, background, fontSize, txt) {
+  var message = "%c" + txt;
+  var style = `color: ${color};`;
+  style += `background: ${background};`;
+  style += `font-size: ${fontSize};`;
+  console.log(message, style);
 }
-letterFinder(`test`, `t`);
+consoleStyler(
+  (color = red),
+  (background = black),
+  (fontSize = 15),
+  (txt = `Hellow Abel`)
+);
