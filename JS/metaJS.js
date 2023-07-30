2f8d3024fed5d6d2b7ddf9c7b0cadec48952d2a2
Abel-B3lla <bel8794@gmail.com>
2023-07-30 17:58:35 +0300

    Class

--- JS/metaJS.js
+++ JS/metaJS.js
@@ -12,16 +12,39 @@
 // }
 // letterFinder(`test`, `t`);
 
-function consoleStyler(color, background, fontSize, txt) {
-  var message = "%c" + txt;
-  var style = `color: ${color};`;
-  style += `background: ${background};`;
-  style += `font-size: ${fontSize};`;
-  console.log(message, style);
+// function consoleStyler(color, background, fontSize, txt) {
+//   var message = "%c" + txt;
+//   var style = `color: ${color};`;
+//   style += `background: ${background};`;
+//   style += `font-size: ${fontSize};`;
+//   console.log(message, style);
+// }
+// consoleStyler(
+//   (color = red),
+//   (background = black),
+//   (fontSize = 15),
+//   (txt = `Hellow Abel`)
+// );
+
+class Chisness {
+  // name of a class start in uppercase
+  constructor(segetoMeter, chisoMeter) {
+    // constructor function, to assign past parameters to future object properties
+    this.segetoMeter = segetoMeter;
+    this.chisoMeter = chisoMeter;
+  }
+  chisCheker() {
+    // just the name of the method needed. no need to state `function`
+    if (this.segetoMeter < 20 && this.chisoMeter > 80) {
+      return `a certified Chis!`;
+    } else {
+      return `a certified Shwaye!`;
+    }
+  }
 }
-consoleStyler(
-  (color = red),
-  (background = black),
-  (fontSize = 15),
-  (txt = `Hellow Abel`)
-);
+// you can now start building objects. (instantiate the object i.e. instances of a given class)
+var faya = new Chisness(19, 81); // using new
+// you can access properties from the newly created object.
+console.log(`Faya is`, faya.chisCheker());
+var aman = new Chisness(33, 67);
+console.log(`Aman is`, aman.chisCheker());
