eb16deff23173a68520e808ff3a53f463b67d4a5
Abel-B3lla <bel8794@gmail.com>
2023-07-30 21:57:18 +0300

    Exam

--- JS/metaJS.js
+++ JS/metaJS.js
@@ -48,3 +48,42 @@ var faya = new Chisness(19, 81); // using new
 console.log(`Faya is`, faya.chisCheker());
 var aman = new Chisness(33, 67);
 console.log(`Aman is`, aman.chisCheker());
+
+// Task 1: Code a Person class
+class Person {
+  constructor(name = `Tom`, age = 20, energy = 100) {
+    this.name = name;
+    this.age = age;
+    this.energy = energy;
+  }
+  sleep() {
+    return this.energy + 10;
+  }
+  doSomethingFun() {
+    return this.energy - 10;
+  }
+}
+// Task 2: Code a Worker class
+class Worker extends Person {
+  constructor(xp = 0, hourlyWage = 10, name, age, energy) {
+    super(name, age, energy);
+    this.xp = xp;
+    this.hourlyWage = hourlyWage;
+  }
+  goToWork() {
+    this.xp = this.xp + 10;
+  }
+}
+// Task 3: Code an intern object, run methods
+function intern() {
+  var intern = new Worker(0, 10, `Bob`, 21, 110);
+  intern.goToWork();
+  return intern;
+}
+
+// Task 4: Code a manager object, methods
+function manager() {
+  var manager = new Worker(100, 30, `Alice`, 30, 120);
+  manager.doSomethingFun();
+  return manager;
+}
